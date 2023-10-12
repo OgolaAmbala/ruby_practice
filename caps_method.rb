5ebcfc45ab9cd08dad04cd465e8c7d533d666334
Ogola Ambala <66788503+OgolaAmbala@users.noreply.github.com>
2023-10-12 12:49:20 +0100

    Completed conditional exercises

---
+++ caps_method.rb
@@ -0,0 +1,7 @@
+def return_new(string)
+    if string.length > 10
+        string.upcase
+    else
+        string
+    end
+end    
---
+++ evaluate_number.rb
@@ -0,0 +1,14 @@
+puts "Enter a number between 0 and 100"
+
+number = gets.chomp.to_i
+
+if number < 0
+    puts "You cannot enter a negative number"
+elsif number <= 50
+    puts "#{number} is between 0 and 50"
+elsif number <= 100 
+        puts "#{number} is between 51 and 100"
+else
+    puts "You did not follow the instructions"
+end
+
